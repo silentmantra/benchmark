adadd1455c605d5a02751f3b9b90a1bfa8eb03be
Alexander Nenashev <silentmantra.ru@gmail.com>
2023-09-28 21:03:04 +0200

    tweak loader

--- loader.js
+++ loader.js
@@ -1,3 +1,6 @@
-const script = document.createElement('script');
-script.src = 'https://raw.githack.com/silentmantra/benchmark/master/benchmark.min.js';
-document.head.appendChild(script);
+{
+    const script = document.createElement('script');
+    script.src = 'https://raw.githack.com/silentmantra/benchmark/master/benchmark.min.js';
+    script.type = 'module';
+    document.head.appendChild(script);
+}
